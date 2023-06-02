1c85a5a3cb5dba85671657ecd6d9ccbe807c71ee
Noridoo25 <115818873+Noridoo25@users.noreply.github.com>
2023-06-03 00:00:59 +0700

    note model

--- src/models/Note.js
+++ src/models/Note.js
@@ -1,6 +1,30 @@
+// decorator-composition tronng structual
 import mongoose from "mongoose";
 
-const NoteSchema = new mongoose.Schema(
+// Decorator: logDecorator
+function logDecorator(schema) {
+  schema.pre("save", function (next) {
+    console.log("Saving note...");
+    next();
+  });
+
+  schema.post("save", function () {
+    console.log("Note saved!");
+  });
+
+  return schema;
+}
+
+// Composition: addValidation
+function addValidation(schema) {
+  schema.path("title").validate(function (value) {
+    return value.length <= 50;
+  }, "Title must not exceed 50 characters");
+
+  return schema;
+}
+
+const NoteSchema = logDecorator(addValidation(new mongoose.Schema(
   {
     title: {
       type: String,
@@ -22,6 +46,6 @@ const NoteSchema = new mongoose.Schema(
   {
     timestamps: true,
   }
-);
+)));
 
 export default mongoose.model("Note", NoteSchema);
